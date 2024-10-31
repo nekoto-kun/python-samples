c9efb0d918278fbc0baea4897e4bd3b891f70908
Andre Kho <andreyonathan65@gmail.com>
2024-10-31 16:03:11 +0700

    Add example of SQL injection vulnerability and protection

---
+++ 05-sql-injection.py
@@ -0,0 +1,42 @@
+# Import libraries
+import sqlite3
+
+# Vulnerable function
+def get_user_data(user_id):
+    query = f"SELECT * FROM users WHERE id = {user_id};"
+    connection = sqlite3.connect("samples/user.sqlite")
+    cursor = connection.cursor()
+
+    cursor.execute(query)
+    user_data = cursor.fetchone()
+
+    cursor.close()
+    connection.close()
+
+    return user_data
+
+# Attack scenario
+print("Vulnerable function")
+user_id = "1 OR 1=1"
+user_data = get_user_data(user_id)
+print(user_data)
+
+# Protection
+def get_user_data_protected(user_id):
+    query = f"SELECT * FROM users WHERE id = ?;"
+    connection = sqlite3.connect("samples/user.sqlite")
+    cursor = connection.cursor()
+
+    cursor.execute(query, (user_id,))
+    user_data = cursor.fetchone()
+
+    cursor.close()
+    connection.close()
+
+    return user_data
+
+# Attack scenario
+print("\nProtected function")
+user_id = "1 OR 1=1"
+user_data = get_user_data_protected(user_id)
+print(user_data)
