f9712dc55c9541658ae4c87b92d3ee9a2eb47c33
Andre Kho <andreyonathan65@gmail.com>
2024-11-02 14:58:04 +0700

    Remove an example

--- 07-psutil.py
+++
@@ -1,7 +0,0 @@
-import psutil
-
-def list_processes():
-    for process in psutil.process_iter(['pid', 'name', 'cmdline']):
-        print(f"PID: {process.info['pid']}, Name: {process.info['name']}, Cmdline: {process.info['cmdline']}")
-
-list_processes()
