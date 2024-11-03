291ee548603fc41d045f73364709d182723687a3
Andre Kho <andreyonathan65@gmail.com>
2024-11-03 15:58:23 +0700

    Add clearer differences between functions

--- 05-sql-injection.py
+++ 05-sql-injection.py
@@ -8,7 +8,7 @@ def get_user_data(user_id):
     cursor = connection.cursor()
 
     cursor.execute(query)
-    user_data = cursor.fetchone()
+    user_data = cursor.fetchall() # Fetch all rows
 
     cursor.close()
     connection.close()
@@ -27,8 +27,9 @@ def get_user_data_protected(user_id):
     connection = sqlite3.connect("samples/user.sqlite")
     cursor = connection.cursor()
 
-    cursor.execute(query, (user_id,))
-    user_data = cursor.fetchone()
+    cursor.execute(query, (user_id,)) # Pass the user_id as a tuple
+    # user_data = cursor.fetchall()
+    user_data = cursor.fetchone() # Fetch only one row
 
     cursor.close()
     connection.close()
